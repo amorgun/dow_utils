ec049cbae875df7f38180a40b2af3e4e2a5b1843
amorgun <alexander.morgun.usu@gmail.com>
2025-08-20 01:48:57 +0300

    Fix cli

--- screen_editor/__main__.py
+++ screen_editor/__main__.py
@@ -7,7 +7,7 @@ from .app import start_editor
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('mod_dir', help='Folder with the mod data', type=pathlib.Path)
-    parser.add_argument('screen_dir', help='Folder with the mod data', type=pathlib.Path, default=None, nargs='?')
+    parser.add_argument('screen_dir', help='Folder with .screen files', type=pathlib.Path, default=None, nargs='?')
     args = parser.parse_args()
     screen_dir = args.screen_dir
     start_editor(args.mod_dir, screen_dir, files_root=pathlib.Path(__file__).parent)
